742b128d899bc99a7205b9ef0e344af0a4e0af0b
ancool <ancool@foxmail.com>
2016-02-26 12:35:58 +0800

    android

--- index.web.js
+++ index.web.js
@@ -1,5 +1,5 @@
-import React, { AppRegistry, Component } from 'react-native'
 import {Provider } from 'react-redux'
+import React, { AppRegistry, Component } from 'react-native'
 import configureStore from './store/configureStore'
 import App from './containers/App.react'
 
